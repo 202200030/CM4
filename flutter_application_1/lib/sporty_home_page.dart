4a8fdfb2814b36fa66fb476446470748410f77bd
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2024-06-23 00:50:33 +0100

    alteração barra

--- flutter_application_1/lib/sporty_home_page.dart
+++ flutter_application_1/lib/sporty_home_page.dart
@@ -51,7 +51,7 @@ class _SportyHomePageState extends State<SportyHomePage> {
       _lifeBar = 100;
     });
 
-    _lifeTimer = Timer.periodic(Duration(seconds: 5), (timer) {
+    _lifeTimer = Timer.periodic(Duration(days:1), (timer) {
       setState(() {
         _decreaseLife();
       });
