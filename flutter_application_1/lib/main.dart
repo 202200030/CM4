9b6fe114ad68fed1d1ecfb585db5cc6def3534f1
rmaduro <202200166@estudantes.ips.pt>
2024-06-22 15:43:46 +0100

    workouts

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sporty_application/workouts_page.dart';
 import 'login_screen.dart';
 import 'sporty_home_page.dart';
 import 'customization_screen.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(), 
         '/home': (context) => SportyHomePage(),
         '/customize': (context) => CustomizationScreen(),
+        '/workouts':(context) => WorkoutsPage(),
       },
     );
   }
