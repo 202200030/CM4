cb7599feaa5b44af2fd2a25774555a68b6543bbe
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2024-06-23 17:13:07 +0100

    achievements

--- flutter_application_1/lib/achievements_screen.dart
+++ flutter_application_1/lib/achievements_screen.dart
@@ -58,13 +58,15 @@ class AchievementsScreen extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         children: [
-          AchievementItem('assets/achievement1.png', 'First Achievement'),
-          AchievementItem('assets/achievement2.png', 'Second Achievement'),
-          AchievementItem('assets/achievement3.png', 'Third Achievement'),
-          AchievementItem('assets/achievement4.png', 'Fourth Achievement'),
-          AchievementItem('assets/achievement5.png', 'Fifth Achievement'),
-          AchievementItem('assets/achievement6.png', 'Sixth Achievement'),
-          AchievementItem('assets/achievement7.png', 'Seventh Achievement'),
+          AchievementItem('assets/gold.png', 'First Achievement'),
+          AchievementItem('assets/gold.png', 'Second Achievement'),
+          AchievementItem('assets/gold.png', 'Third Achievement'),
+          AchievementItem('assets/silver.png', 'Fourth Achievement'),
+          AchievementItem('assets/silver.png', 'Fifth Achievement'),
+          AchievementItem('assets/silver.png', 'Sixth Achievement'),
+          AchievementItem('assets/bronze.png', 'Seventh Achievement'),
+          AchievementItem('assets/bronze.png', 'Seventh Achievement'),
+          AchievementItem('assets/bronze.png', 'Seventh Achievement')
         ],
       ),
     );
