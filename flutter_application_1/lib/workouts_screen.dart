cb74234405ad81fb12e7547604b4ace60c6d30fc
rmaduro <202200166@estudantes.ips.pt>
2024-06-24 00:51:08 +0100

    mapa feito

--- flutter_application_1/lib/workouts_screen.dart
+++ flutter_application_1/lib/workouts_screen.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'workout_in_progress_screen.dart'; 
 
 class WorkoutsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            buildTitleRow(),
-            SizedBox(height: 20),
-            buildCenteredText('Select your workout:'),
-            SizedBox(height: 20),
-            buildWorkoutGrid(context), 
-          ],
-        ),
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        buildTitleRow(),
+        SizedBox(height: 20),
+        buildCenteredText('Select your workout:'),
+        SizedBox(height: 20),
+        buildWorkoutGrid(context), // Pass context to buildWorkoutGrid
+      ],
     );
   }
 
@@ -56,105 +51,38 @@ class WorkoutsPage extends StatelessWidget {
   Expanded buildWorkoutGrid(BuildContext context) {
     return Expanded(
       child: GridView.count(
-        crossAxisCount: 3,
+        crossAxisCount: 2, // Changed to 2 columns for running and walking
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         children: [
-          WorkoutItem('assets/Workouts_Assests/caminhada.png', 'Walk', () {
-            _startWorkout(context);
+          WorkoutItem('assets/Workouts_Assests/corrida.png', () {
+            Navigator.pop(context, 'assets/Workouts_Assests/corrida.png');
           }),
-          WorkoutItem('assets/Workouts_Assests/corrida.png', 'Run', () {
-            _startWorkout(context);
-          }),
-          WorkoutItem('assets/Workouts_Assests/cycling.png', 'Cycling', () {
-            _startWorkout(context);
-          }),
-          WorkoutItem('assets/Workouts_Assests/gym.png', 'Gym', () {
-            _startWorkout(context);
-          }),
-          WorkoutItem('assets/Workouts_Assests/desportos.png', 'Sports', () {
-            _startWorkout(context);
-          }),
-          WorkoutItem('assets/Workouts_Assests/Basketball.png', 'Basketball', () {
-            _startWorkout(context);
-          }),
-          WorkoutItem('assets/Workouts_Assests/free_training.png', 'Free Training', () {
-            _startWorkout(context);
+          WorkoutItem('assets/Workouts_Assests/caminhada.png', () {
+            Navigator.pop(context, 'assets/Workouts_Assests/caminhada.png');
           }),
         ],
       ),
-    );
-  }
-
-  void _startWorkout(BuildContext context) {
-    Navigator.pop(context);
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => WorkoutInProgressScreen()),
     );
   }
 }
 
 class WorkoutItem extends StatelessWidget {
   final String assetPath;
-  final String label;
   final VoidCallback onPressed;
 
-  WorkoutItem(this.assetPath, this.label, this.onPressed);
+  WorkoutItem(this.assetPath, this.onPressed);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        showModalBottomSheet(
-          context: context,
-          backgroundColor: Colors.transparent,
-          builder: (BuildContext context) {
-            return Container(
-              height: MediaQuery.of(context).size.height * 0.7, // Adjust height as needed
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                    ),
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Image.asset(assetPath, width: 70),
-                        SizedBox(height: 20),
-                        Text(
-                          label,
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: onPressed,
-                          child: Text('Start Workout'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        );
-      },
+      onTap: onPressed,
       child: Column(
         children: [
           Image.asset(assetPath, width: 70),
           SizedBox(height: 5),
           Text(
-            label,
+            _getWorkoutLabel(assetPath), // Assuming a function to get label based on assetPath
             style: TextStyle(
               fontSize: 16,
               color: Colors.grey[700],
@@ -162,6 +90,20 @@ class WorkoutItem extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  String _getWorkoutLabel(String assetPath) {
+    // Implement logic to derive label from assetPath
+    return assetPath.replaceAll('assets/Workouts_Assests/', '').replaceAll('.png', '');
+  }
+}
+
+class BottomSheetWorkouts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: WorkoutsPage(),
     );
   }
 }
